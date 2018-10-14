dbef98817ba80e570fdc3aa55c4ba9f440b73494
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-10-14 12:33:18 -0400

    Added vote count to article list

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -1,42 +1,65 @@
 var db = require("../models");
 
+function renderArticleList(res, data, nextPage) {
+  if (!data.length) {
+    return res.render("404", {
+      text: "There are no articles to display."
+    });
+  }
+  var toRender = { articles: data };
+  for(var i = 0; i < data.length; i++){
+    var sad = 0, fake = 0, boring = 0;
+    for(var j = 0 ; j < data[i].votes.length; j++){
+      if(data[i].votes[j].text === "sad"){
+        sad++;
+      }
+      else if(data[i].votes[j].text === "fake"){
+        fake++;
+      }
+      else if(data[i].votes[j].text === "boring"){
+        boring++;
+      }
+    }
+    data[i].sad = sad;
+    data[i].fake = fake;
+    data[i].boring = boring;
+  }
+  if (data.length === 25) { toRender.nextPage = nextPage; }
+  res.render("index", toRender);
+}
+
 module.exports = function (app) {
-  app.get("/", function(req, res){
+  app.get("/", function (req, res) {
     db.Article.find({}, null, {
-      limit: 25, 
+      limit: 25,
       sort: {
         updated: -1
       }
     })
+      .populate("votes")
       .then(function(data){
-        if(!data.length){
-          return res.render("404", {
-            text: "There are no articles to display."
-          });
-        }
-        var toRender = { articles: data };
-        if(data.length === 25) { toRender.nextPage = 2; }
-        res.render("index", toRender);
+        renderArticleList(res, data, 2);
       });
   });
 
-  app.get("/articles/:page", function(req, res){
+  app.get("/articles/page/:page", function (req, res) {
     db.Article.find({}, null, {
-      limit: 25, 
-      skip: 25 * (parseInt(req.params.page)-1),
+      limit: 25,
+      skip: 25 * (parseInt(req.params.page) - 1),
       sort: {
         updated: -1
       }
     })
-    .then(function(data){
-      if(!data.length){
-        return res.render("404", {
-          text: "There are no articles to display."
-        });
-      }
-      var toRender = { articles: data };
-      if(data.length === 25) { toRender.nextPage = (parseInt(req.params.page)+1); }
-      res.render("index", toRender);
-    });
+      .populate("votes")
+      .then(function(data){
+        renderArticleList(res, data, (parseInt(req.params.page) + 1));
+      });
   });
+
+  app.get("/articles/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .then(function (data) {
+
+      })
+  })
 }
